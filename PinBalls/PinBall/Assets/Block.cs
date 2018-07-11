6e8fec8c23cc9eb2746f127b060253dfbadeccee
Hiroto-Nakanishi <mode777@hotmail.com>
2018-07-12 04:00:09 +0900

    ピンボール完成品

---
+++ PinBalls/PinBall/Assets/Block.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+    GameObject scoreUI;
+
+    public int a;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreUI = GameObject.Find("Score");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //衝突判定
+        if (collision.gameObject.tag == "Ball")
+        {
+            //スコア処理を追加
+            scoreUI.GetComponent<Score>().AddPoint(a);
+
+        }
+    }
+
+}
---
+++ PinBalls/PinBall/Assets/Score.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    // スコアを表示する
+    public Text scoreText;
+
+    // スコア
+    private int score;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Update()
+    {
+        // スコア・ハイスコアを表示する
+        scoreText.text = "Score: " +score.ToString();
+    }
+
+    // ゲーム開始前の状態に戻す
+    private void Initialize()
+    {
+        // スコアを0に戻す
+        score = 0;
+
+    }
+
+    // ポイントの追加
+    public void AddPoint(int point)
+    {
+        score = score + point;
+    }
+
+}
